f022a698b688606c4d2aba237eaa6999c6c3dbb1
Fedor Sarafanov <sfg180@yandex.ru>
2017-11-30 22:05:06 +0300

    Auto stash before merge of "master" and "origin/conclusion"

--- plot_ris6.py
+++ plot_ris6.py
@@ -8,25 +8,26 @@ from numpy import arctan
 pylab.rc('text', usetex=True)
 pylab.rc('font', family='serif')
 
-omega=np.logspace(0, 7, num=400)
+omega=np.logspace(0, 7, num=4000)
 
 C=5e-8
-R=26600
+R=13000
 O=omega*R*C
 
-phi=abs(arctan((1-6*O**2)/(O*(5-O**2))))
-fi = np.loadtxt('data/chem6.tsv', skiprows=1, delimiter='\t', usecols=(0, 6))
+# phi=abs((1-4*O**2)/np.sqrt((1-4*O**2)**2+64*O**2))
+phi=arctan(8*O/(1-4*O**2))
+# fi = np.loadtxt('data/chem6.tsv', skiprows=1, delimiter='\t', usecols=(0, 6))
 # print(fi)
 
 # Результаты эксперимента
-om=fi.T[0]*2*np.pi
-fi=fi.T[1]
+# om=fi.T[0]*2*np.pi
+# fi=fi.T[1]
 
 pylab.semilogx(omega, phi, "r-")
-pylab.semilogx(om, fi, "bo", markersize=5)
+# pylab.semilogx(om, fi, "bo", markersize=5)
 
-pylab.xlabel(r"$\omega$, Hz")
-pylab.ylabel(r"$\phi$, rad")
+pylab.xlabel(r"$\omega$")
+pylab.ylabel(r"$\phi$")
 
 pylab.grid(True)
 pylab.show()
